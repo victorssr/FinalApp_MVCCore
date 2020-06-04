1bdfcb58e8a1c433a3c5d4e37c8fe45459ba1edb
Victor Savoi <savoi.victor@hotmail.com>
2020-06-04 16:20:05 -0300

    Add supress elements by claim with tag helper

---
+++ src/VSDev.MVC/Extensions/TagHelpers/SupressElementByClaimTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace VSDev.MVC.Extensions.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-value")]
+    public class SupressElementByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public SupressElementByClaimTagHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("supress-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            var possuiAcesso = CustomAuthorization.ValidarClaimUsuario(_httpContextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+            if (possuiAcesso) return;
+
+            output.SuppressOutput();
+        }
+    }
+}
