6a9717259a69509b969e0449eb562847b4c62acf
Victor Savoi <savoi.victor@hotmail.com>
2020-06-04 16:26:18 -0300

    Add Supress by action with tag helper

---
+++ src/VSDev.MVC/Extensions/TagHelpers/SupressElementByActionTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace VSDev.MVC.Extensions.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = "supress-by-action")]
+    public class SupressElementByActionTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public SupressElementByActionTagHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-action")]
+        public string ActionName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            var currentAction = _httpContextAccessor.HttpContext.GetRouteData().Values["action"].ToString();
+            if (ActionName.Contains(currentAction)) return;
+
+            output.SuppressOutput();
+        }
+    }
+}
